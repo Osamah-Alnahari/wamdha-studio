6c6cbfad875d4ecaa09c0d68a78d3e09dddb8469
Lixsr <osama142494@gmail.com>
2025-04-28 20:03:17 +0300

    aws auth

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,5 +1,4 @@
 "use client";
-
 import { useState } from "react";
 import { usePathname, useRouter } from "next/navigation";
 import { Book, FileText, LogOut, Menu, X, PlusCircle } from "lucide-react";
