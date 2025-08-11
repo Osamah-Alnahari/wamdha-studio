04ed7d3839466d0006598fa8bfb286e32ef92506
Lixsr <osama142494@gmail.com>
2025-08-11 22:00:43 +0300

    protect auth routes

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,5 +1,13 @@
+"use client";
 import { LoginForm } from "@/components/auth/login-form";
+import { useAuth } from "@/contexts/AuthContext";
+import { redirect } from "next/navigation";
+
 
 export default function LoginPage() {
+
+  if ((useAuth())?.user?.isLoggedIn) {
+    redirect("/books");
+  }
   return <LoginForm />;
 }
--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,5 +1,11 @@
+"use client";
 import { RegisterForm } from "@/components/auth/register-form";
+import { useAuth } from "@/contexts/AuthContext";
+import { redirect } from "next/navigation";
 
 export default function RegisterPage() {
+  if (useAuth()?.user?.isLoggedIn) {
+    redirect("/books");
+  }
   return <RegisterForm />;
 }
